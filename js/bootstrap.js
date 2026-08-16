a7ecd10daf1ddae27c76857ae73cb1f1fef0f367
Zes45-ux <170490392+Zes45-ux@users.noreply.github.com>
2026-08-17 02:03:42 +0800

    feat: add Vercel analytics and speed insights

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -1,3 +1,9 @@
+import { inject as injectAnalytics } from "@vercel/analytics";
+import { injectSpeedInsights } from "@vercel/speed-insights";
+
+injectAnalytics();
+injectSpeedInsights();
+
 // A dependency graph that contains any wasm must all be imported
 // asynchronously. This `bootstrap.js` file does the single async import, so
 // that no one else needs to worry about it again.
